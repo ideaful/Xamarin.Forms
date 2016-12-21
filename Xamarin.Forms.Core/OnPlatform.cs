ee25cd5a2f18452641c30ca3f3c25e6954a72925
Stephane Delcroix <stephane@delcroix.org>
2016-12-21 17:14:54 +0100

    [C] Obsolete TargetPlatform

--- Xamarin.Forms.Core/OnPlatform.cs
+++ Xamarin.Forms.Core/OnPlatform.cs
@@ -1,31 +1,87 @@
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+
 namespace Xamarin.Forms
 {
+	[ContentProperty("Platforms")]
 	public class OnPlatform<T>
 	{
-		public T Android { get; set; }
+		public OnPlatform()
+		{
+			Platforms = new List<On>();
+		}
 
-		public T iOS { get; set; }
+		bool useFallback;
+		T android;
+		[Obsolete]
+		public T Android {
+			get { return android; }
+			set {
+				useFallback = true;
+				android = value;
+			}
+		}
 
-		public T WinPhone { get; set; }
+		T ios;
+		[Obsolete]
+		public T iOS {
+			get { return ios; }
+			set {
+				useFallback = true;
+				ios = value;
+			}
+		}
 
-		public T Tizen { get; set; }
+		T winPhone;
+		[Obsolete]
+		public T WinPhone {
+			get { return winPhone; }
+			set {
+				useFallback = true;
+				winPhone = value;
+			}
+		}
+
+		public IList<On> Platforms { get; private set;}
+
+		static readonly IValueConverterProvider s_valueConverter = DependencyService.Get<IValueConverterProvider>();
 
 		public static implicit operator T(OnPlatform<T> onPlatform)
 		{
-			switch (Device.OS)
-			{
-				case TargetPlatform.iOS:
-					return onPlatform.iOS;
-				case TargetPlatform.Android:
-					return onPlatform.Android;
-				case TargetPlatform.Windows:
-				case TargetPlatform.WinPhone:
-					return onPlatform.WinPhone;
-				case TargetPlatform.Tizen:
-					return onPlatform.Tizen;
+			foreach (var onPlat in onPlatform.Platforms) {
+				if (onPlat.Platform == null)
+					continue;
+				if (!onPlat.Platform.Contains(Device.RuntimePlatform))
+					continue;
+				if (s_valueConverter == null)
+					continue;
+				return (T)s_valueConverter.Convert(onPlat.Value, typeof(T), null, null);
 			}
 
-			return onPlatform.iOS;
+			if (!onPlatform.useFallback)
+				return default(T);
+
+			//legacy fallback
+#pragma warning disable 0612
+			switch (Device.RuntimePlatform) {
+			case Device.iOS:
+			default:
+				return onPlatform.iOS;
+			case Device.Android:
+				return onPlatform.Android;
+			case Device.Windows:
+			case Device.WinPhone:
+				return onPlatform.WinPhone;
+#pragma warning restore 0612
+			}
 		}
+	}
+
+	[ContentProperty("Value")]
+	public class On
+	{
+		public IList<string> Platform { get; set; }
+		public object Value { get; set; }
 	}
 }
